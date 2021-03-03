28e11fdad8cd4bc5d1bcd3b93ba43fe005323325
jvshoffmann <jvshoffmann@gmail.com>
2021-03-03 10:07:32 -0300

    all tests passed

--- src/App.js
+++ src/App.js
@@ -1,75 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import drumKeys from './drumData.json'
 
-const drumKeys = [
-  {
-    key:"Q",
-    keyCode: 81,
-    id: 'Heater-1',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
-  },
-  {
-    key:"W",
-    keyCode: 87,
-    id: 'Heater-2',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
-  },
-  { 
-    key:"E",
-    keyCode: 69,
-    id: 'Heater-3',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
-  },
-  {
-    key:"A",
-    keyCode: 65,
-    id: 'Heater-4',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'}
-    ,
-  {
-    key:"S",
-    keyCode: 83,
-    id: 'Clap',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
-  },
-  {
-    key:"D",
-    keyCode: 68,
-    id: 'Open-HH',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
-  },
-  {
-    key:"Z",
-    keyCode: 90,
-    id: "Kick-n'-Hat",
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
-  },
-  {
-    key:"X",
-    keyCode: 88,
-    id: 'Kick',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
-  },
-  {
-    key:"C",
-    keyCode: 67,
-    id: 'Closed-HH',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
-  }
-]
-
-const simulateClickOrder = ['mousedown', 'mouseup'];
-
-class App extends React.Component {
+class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      lastAudio: ""
+      lastAudio: " "
     }
-    this.playAudio = this.playAudio.bind(this);
   }
  
-  playAudio(audioId, lastAudio){
+  playAudio = (audioId, lastAudio)=>{
     let drumSound = document.getElementById(audioId)
     this.setState({lastAudio:lastAudio})
     drumSound.play();
